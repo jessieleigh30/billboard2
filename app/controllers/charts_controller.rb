f8c2ede491ef285022c4e6e5a164a86c0b2b93d5
Jessica Anderson <jessie.leigh30@gmail.com>
2018-11-18 20:47:07 -0700

    days work

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,13 +1,50 @@
 class ChartsController < ApplicationController
+
+  before_action :set_chart, only: [:show, :update, :edit, :destroy]
+
   def index
+    @charts = Chart.all
   end
 
   def show
   end
 
   def new
+    @chart = Chart.new
   end
 
   def edit
+  end
+
+  def create
+    @chart = Chart.new(params)
+    if @chart.save
+      redirect_to charts_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @chart.update(chart_params)
+      redirect_to chart_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @chart.destroy
+    redirect to chart_path
+  end
+
+  private
+
+  def set_chart
+    @chart = Chart.find(params[:id])
+  end
+
+  def chart_params
+    params.require(:chart).permit(:name)
   end
 end
